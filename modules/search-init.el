931a4f79894be81a6d3fd44bfc2cdc915a57f32e
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2015-04-04 23:31:30 -0400

    Require search-init.

---
+++ modules/search-init.el
@@ -0,0 +1,27 @@
+;;; search-init.el --- Part of emacs initialization  -*- lexical-binding: t; -*- -*- no-byte-compile: t; -*-
+
+;;; Commentary:
+;; Configure search and replace.
+
+;;; Code:
+
+(use-package isearch
+  :defer t
+  :config
+  (setq search-highlight t) ; highlight incremental search
+  :diminish isearch-mode)
+
+(use-package replace
+  :defer t
+  :config
+  (setq query-replace-highlight t)) ; highlight during query
+
+(setq case-fold-search t) ; make search ignore case
+
+(use-package swiper
+  :ensure t
+  :disabled t)
+
+(provide 'search-init)
+
+;;; search-init.el ends here
