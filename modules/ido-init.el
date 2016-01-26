11f38290401e515034fc7e4cbd5bbf55ac57b1de
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2016-01-26 15:10:31 -0500

    Fixes to ido.

--- modules/ido-init.el
+++ modules/ido-init.el
@@ -14,32 +14,31 @@
   (setq ido-enable-flex-matching t
         ido-enable-prefix nil
         ido-max-prospects 10
-        ido-case-fold t
-        ido-use-filename-at-point nil ; other options: 'ffap-guesser, 'guess
-        ido-show-dot-for-dired nil ; don't show current directory as the first choice
-        ido-create-new-buffer 'always ; other options: prompt, never
+        ido-case-fold t ; Searching of buffer and file names should ignore case
+        ido-use-filename-at-point nil ; Other options: 'ffap-guesser, 'guess
+        ido-show-dot-for-dired nil ; Don't show current directory as the first choice
+        ido-create-new-buffer 'always ; Other options: prompt, never
         ido-default-file-method 'selected-window
         ido-save-directory-list-file (concat dotemacs-temp-directory ".ido.last")
         ido-enable-last-directory-history t
-        ido-max-work-directory-list 20
+        ido-max-work-directory-list 50
         ido-max-work-file-list 50
         confirm-nonexistent-file-or-buffer nil
-        ido-use-faces nil ; disable ido faces to see flx highlights
+        ido-use-faces nil ; Disable ido faces to see flx highlights
         ido-use-virtual-buffers 'auto
         ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*" "*Compile-Log*" "Flycheck error messages*"
                              ;; "*Messages*"
                              "Async Shell Command" "*Paradox Report*")
         ido-confirm-unique-completion t
         ido-enable-tramp-completion t
-        ido-ignore-extensions t)
+        ido-ignore-extensions t
+        ido-ignore-files (append '("GTAGS" "GPATH" "GRTAGS" "GSYMS" "TAGS")
+                                 ido-ignore-files))
 
-  (setq ido-ignore-files
-        (append '("GTAGS" "GPATH" "GRTAGS" "GSYMS" "TAGS")
-                ido-ignore-files))
-  (dolist (dirs '(".svn" ".dropbox .git"))
+  (dolist (dirs '(".svn" ".dropbox .git .hg"))
     (add-to-list 'ido-ignore-directories dirs))
 
-  (use-package ido-yes-or-no ; overkill
+  (use-package ido-yes-or-no ; Overkill
     :ensure t
     :disabled t
     :init (ido-yes-or-no-mode))
@@ -49,18 +48,18 @@
     :functions ido-hacks-mode
     :config (ido-hacks-mode))
 
-  (use-package ido-ubiquitous ; allow ido-style completion in more places
+  (use-package ido-ubiquitous ; Allow ido-style completion in more places
     :ensure t
     :init (ido-ubiquitous-mode 1))
 
   (use-package ido-completing-read+
     :ensure t)
 
-  (or (use-package flx-ido ; smarter fuzzy matching for ido
+  (or (use-package flx-ido ; Smarter fuzzy matching for ido
         :ensure t
         :init (flx-ido-mode 1))
 
-      (use-package ido-better-flex ; can add more noise while matching patterns
+      (use-package ido-better-flex ; Can add more noise while matching patterns
         :ensure t
         :disabled t
         :init (ido-better-flex/enable)))
@@ -70,7 +69,8 @@
     :init (ido-at-point-mode 1))
 
   (use-package ido-describe-bindings
-    :ensure t)
+    :ensure t
+    :disabled t)
 
   (cond ((eq dotemacs-ido-view-mode 'vertical) (use-package ido-vertical-mode
                                                  :ensure t
@@ -82,11 +82,7 @@
         ((eq dotemacs-ido-view-mode 'grid)   (use-package ido-grid-mode
                                                :ensure t
                                                :init (ido-grid-mode 1)
-                                               :config
-                                               (setq ido-grid-mode-order t
-                                                     ;;ido-grid-mode-max-rows 3
-                                                     ;;ido-grid-mode-max-columns 6
-                                                     ))))
+                                               :config (setq ido-grid-mode-order t))))
 
   :bind
   (("<f3>" . ido-find-file)
