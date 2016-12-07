d9da40abc4d4de40c270a37e21044d1eab283df3
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2016-12-07 09:35:19 -0600

    python package

--- modules/python-init.el
+++ modules/python-init.el
@@ -6,7 +6,7 @@
 ;;; Code:
 
 ;; Install the following packages
-;; sudo -H pip3 install --upgrade pip numpy scipy psutil django setuptools jedi paramiko cffi rope importmagic yapf pyflakes flake8 importmagic autopep8 pep8 pylint
+;; sudo -H pip3 install --upgrade pip numpy scipy psutil django setuptools jedi paramiko cffi rope importmagic yapf pyflakes flake8 importmagic autopep8 pep8 pylint overrides
 
 (setenv "PYTHONPATH" "python3")
 
