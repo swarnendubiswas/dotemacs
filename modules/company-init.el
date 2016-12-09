4e43a5fe46b3793ce9778c6e84d927e99aae3f68
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2016-12-08 22:56:07 -0600

    company simplification

--- modules/company-init.el
+++ modules/company-init.el
@@ -27,48 +27,26 @@
         company-tooltip-align-annotations t
         company-tooltip-limit 20
         company-selection-wrap-around t
-        company-dabbrev-downcase nil ; Do not downcase the returned candidates
-        company-dabbrev-code-everywhere t ; Offer completions in comments and strings
-        ;; https://github.com/company-mode/company-mode/wiki/Switching-from-AC
-        company-begin-commands '(self-insert-command)
+        ;; company-dabbrev-downcase nil ; Do not downcase the returned candidates
+        ;; company-dabbrev-code-everywhere t ; Offer completions in comments and strings
+        ;; ;; https://github.com/company-mode/company-mode/wiki/Switching-from-AC
+        ;; company-begin-commands '(self-insert-command)
         ;; https://github.com/company-mode/company-mode/issues/123
         company-frontends '(company-pseudo-tooltip-unless-just-one-frontend
                             company-preview-frontend
                             company-echo-metadata-frontend)
         company-require-match 'never)
 
-  ;; ;; https://github.com/company-mode/company-mode/issues/87
-  ;; (defadvice company-pseudo-tooltip-unless-just-one-frontend
-  ;;     (around only-show-tooltip-when-invoked activate)
-  ;;   (when (company-explicit-action-p)
-  ;;     ad-do-it))
-
-  ;; https://github.com/company-mode/company-mode/issues/180
-  (when (bound-and-true-p dotemacs-use-fci-p)
-    (defvar-local company-fci-mode-on-p nil)
-
-    (defun company-turn-off-fci (&rest ignore)
-      (when (boundp 'fci-mode)
-        (setq company-fci-mode-on-p fci-mode)
-        (when fci-mode (fci-mode -1))))
-
-    (defun company-maybe-turn-on-fci (&rest ignore)
-      (when company-fci-mode-on-p (fci-mode 1)))
-
-    (add-hook 'company-completion-started-hook 'company-turn-off-fci)
-    (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
-    (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci))
-
-  (cond ((eq dotemacs-selection 'ivy) (with-eval-after-load "counsel"
-                                        (bind-key [remap complete-symbol] #'counsel-company company-mode-map)
-                                        (bind-key [remap completion-at-point] #'counsel-company company-mode-map)
-                                        (bind-key "C-:" #'counsel-company company-mode-map)
-                                        (bind-key "C-:" #'counsel-company company-active-map)))
-        ((eq dotemacs-selection 'helm) (with-eval-after-load "helm-company"
-                                         (bind-key [remap complete-symbol] #'helm-company company-mode-map)
-                                         (bind-key [remap completion-at-point] #'helm-company company-mode-map)
-                                         (bind-key "C-:" #'helm-company company-mode-map)
-                                         (bind-key "C-:" #'helm-company company-active-map))))
+  ;; (cond ((eq dotemacs-selection 'ivy) (with-eval-after-load "counsel"
+  ;;                                       (bind-key [remap complete-symbol] #'counsel-company company-mode-map)
+  ;;                                       (bind-key [remap completion-at-point] #'counsel-company company-mode-map)
+  ;;                                       (bind-key "C-:" #'counsel-company company-mode-map)
+  ;;                                       (bind-key "C-:" #'counsel-company company-active-map)))
+  ;;       ((eq dotemacs-selection 'helm) (with-eval-after-load "helm-company"
+  ;;                                        (bind-key [remap complete-symbol] #'helm-company company-mode-map)
+  ;;                                        (bind-key [remap completion-at-point] #'helm-company company-mode-map)
+  ;;                                        (bind-key "C-:" #'helm-company company-mode-map)
+  ;;                                        (bind-key "C-:" #'helm-company company-active-map))))
   :bind (:map company-active-map
               ("C-n" . company-select-next)
               ("C-p" . company-select-previous)
@@ -78,9 +56,9 @@
   :ensure t
   :after company
   :config
-  (add-hook 'company-mode-hook
-            (lambda ()
-              (add-to-list 'company-backends 'company-capf)))
+  ;; (add-hook 'company-mode-hook
+  ;;           (lambda ()
+  ;;             (add-to-list 'company-backends 'company-capf)))
   (setq company-flx-limit 50)
   (company-flx-mode 1))
 
