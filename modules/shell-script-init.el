1c53553853743bc65c5d1fed1cf041afc30c0b2e
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2017-01-11 22:10:11 -0600

    made shell script separate

---
+++ modules/shell-script-init.el
@@ -0,0 +1,38 @@
+;;; shell-script-init.el --- Part of Emacs initialization  -*- lexical-binding: t; no-byte-compile: nil; -*-
+
+;;; Commentary:
+;; Configure editing shell scripts.
+
+;;; Code:
+
+(use-package sh-script ; Shell script mode
+  :mode ("\\.zsh\\'" . sh-mode)
+  :config
+  (add-to-list 'auto-mode-alist '("\\bashrc\\'" . sh-mode))
+  (setq sh-basic-offset 4
+        sh-indent-comment t
+        sh-indentation 4
+        sh-indent-after-continuation 'always)
+  ;; Was bound to sh-cd-here
+  (unbind-key "C-c C-d" sh-mode-map)
+
+  (use-package company-shell
+    :ensure t
+    :if (eq dotemacs-completion-in-buffer 'company)
+    :after company
+    :config
+    (setq company-shell-delete-duplicates t)
+    (add-to-list 'company-backends 'company-shell)
+    (add-to-list 'company-backends 'company-fish-shell))
+
+  (use-package company-tern
+    :ensure t
+    :if (eq dotemacs-completion-in-buffer 'company)
+    :after company
+    :config (add-to-list 'company-backends 'company-tern)))
+
+(use-package fish-mode
+  :ensure t
+  :mode ("\\.fish$" . fish-mode))
+
+;;; shell-script-init.el ends here
