587f741dc013c94bd17842e6dcb6af33d7b49ad8
Swarnendu Biswas <swarnendu_cse@yahoo.com>
2017-01-13 12:16:34 -0600

    fixes

--- modules/flycheck-init.el
+++ modules/flycheck-init.el
@@ -6,25 +6,21 @@
 ;;; Code:
 
 (defvar dotemacs-selection)
-(defvar flycheck-completion-system)
 
-;; Flycheck requires gcc 4.8 at least
 (use-package flycheck
   :ensure t
   :diminish flycheck-mode
-  :init (add-hook 'prog-mode-hook #'global-flycheck-mode) ; Enable where possible
+  :init (add-hook 'prog-mode-hook #'global-flycheck-mode)
   :config
   (setq-default flycheck-disabled-checkers '(tex-chktex tex-lacheck)) ; Leave out LaTeX
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list
-        flycheck-standard-error-navigation nil
-        flycheck-check-syntax-automatically '(save mode-enabled))
+        ;; flycheck-display-error-at-point-timer 0.5
+        flycheck-display-errors-delay 0.5
+        ;;       flycheck-standard-error-navigation nil
+        )
 
-  (cond ((eq dotemacs-selection 'helm) (setq flycheck-completion-system 'helm))
-        ((eq dotemacs-selection 'ido)  (setq flycheck-completion-system 'ido)))
-
-  (use-package flycheck-pos-tip ; Show flycheck messages in popups
+  (use-package flycheck-pos-tip ; Show error messages in popups, but hinders visibility
     :ensure t
-    :functions flycheck-pos-tip-error-messages
     :init (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
 (use-package flycheck-irony
