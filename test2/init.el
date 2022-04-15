60575a656f047b169147362507d42670fd86afe3
Swarnendu Biswas <7343807+swarnendubiswas@users.noreply.github.com>
2022-04-15 08:41:20 +0530

    more progress

--- test2/init.el
+++ test2/init.el
@@ -588,6 +588,170 @@ Corfu does not support TUI, so we have to fallback on company."
 
 
 
+(use-package number-separator
+  :straight nil
+  :load-path "extras"
+  :commands number-separator-mode
+  :disabled t
+  :diminish
+  :custom
+  (number-separator ",")
+  (number-separator-interval 3)
+  (number-separator-ignore-threshold 4)
+  (number-separator-decimal-char "."))
+
+(use-package highlight-escape-sequences
+  :straight t
+  :commands hes-mode
+  :hook (prog-mode-hook . hes-mode))
+
+;; First mark the word, then add more cursors. Use `mc/edit-lines' to add a cursor to each line in
+;; an active region that spans multiple lines.
+(use-package multiple-cursors
+:straight t
+  :bind
+  (("C-<"     . mc/mark-previous-like-this)
+   ("C->"     . mc/mark-next-like-this)
+   ("C-c C-<" . mc/mark-all-like-this)))
+
+;; Edit remote file: "/method:user@host#port:filename". Shortcut "/ssh::" will connect to default
+;; "user@host#port".
+;; Edit local file with sudo: "C-x C-f /sudo::/etc/hosts".
+;; Open a remote file with ssh + sudo: "C-x C-f /ssh:host|sudo:root:/etc/passwd".
+;; Multihop syntax: "C-x C-f /ssh:bird@bastion|ssh:you@remotehost:/path"
+;; Multihop with sudo: "C-x C-f /ssh:you@remotehost|sudo:remotehost:/path/to/file"
+;; Multihop with sudo with custom user: "C-x C-f
+;; /ssh:you@remotehost|sudo:them@remotehost:/path/to/file"
+
+;; https://helpdeskheadesk.net/help-desk-head-desk/2021-05-19/ Use bookmarks to speed up remote file
+;; access: upon visiting a location with TRAMP, save it as a bookmark with `bookmark-set' ("C-x r
+;; m"). To revisit that bookmark, use `bookmark-jump' ("C-x r b") or `bookmark-bmenu-list' ("C-x r
+;; l"). Rename the bookmarked location in `bookmark-bmenu-mode' with `R'.
+(use-package tramp
+  :straight nil
+  :defines tramp-ssh-controlmaster-options
+  :config
+  (setq tramp-default-user user-login-name
+        ;; Tramp uses SSH when connecting and when viewing a directory, but it will use SCP to copy
+        ;; files which is faster than SSH.
+        ;; tramp-default-method "ssh"
+        tramp-default-remote-shell "/usr/bin/bash"
+        remote-file-name-inhibit-cache nil ; Remote files are not updated outside of Tramp
+        ;; Disable default options, reuse SSH connections by reading "~/.ssh/config" control master
+        ;; settings
+        ;; https://emacs.stackexchange.com/questions/22306/working-with-tramp-mode-on-slow-connection-emacs-does-network-trip-when-i-start
+        ;; https://puppet.com/blog/speed-up-ssh-by-reusing-connections
+        tramp-ssh-controlmaster-options ""
+        tramp-verbose 1
+        ;; Disable version control for remote files to improve performance
+        vc-ignore-dir-regexp (format "\\(%s\\)\\|\\(%s\\)"
+                                     vc-ignore-dir-regexp tramp-file-name-regexp))
+
+  (defalias 'exit-tramp 'tramp-cleanup-all-buffers)
+
+  ;; Disable backup
+  (add-to-list 'backup-directory-alist (cons tramp-file-name-regexp nil))
+
+  ;; Include this directory in $PATH on remote
+  (add-to-list 'tramp-remote-path (expand-file-name ".local/bin" (getenv "HOME")))
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+  ;; https://www.gnu.org/software/tramp/
+  (setq debug-ignored-errors (cons 'remote-file-error debug-ignored-errors))
+  :bind ("C-S-q" . tramp-cleanup-all-buffers))
+
+;; (declare-function sb/sshlist "private")
+
+;; (progn
+;;   (defun sb/ivy-tramp ()
+;;     "Invoke remote hosts with ivy and tramp."
+;;     (interactive)
+;;     (counsel-find-file (ivy-read "Remote Tramp targets: " (sb/sshlist))))
+
+;;   (bind-key "C-c d t" #'sb/ivy-tramp))
+
+(use-package counsel-tramp
+  :straight t
+  :if (eq sb/minibuffer-completion 'ivy)
+  :bind ("C-c d t" . counsel-tramp))
+
+
+;; TODO: SSH into Gcloud
+;; https://gist.github.com/jackrusher/36c80a2fd6a8fe8ddf46bc7e408ae1f9
+;; Make sure you have set your default project with:
+;; "gcloud config set project <project-name>"
+;; "C-x C-f /gcssh:compute-instance:/path/to/filename.clj"
+
+;; LATER: Can we shorten long Tramp file names? This does not work with Tramp.
+;; (add-to-list 'directory-abbrev-alist
+;;              '("/ssh:swarnendu@vindhya.cse.iitk.ac.in:/data/swarnendu/" . "/vindhya/data/swarnendu/"))
+;; (add-to-list 'directory-abbrev-alist
+;;              '("/ssh:swarnendu@vindhya.cse.iitk.ac.in:/home/swarnendu/" . "/vindhya/home/swarnendu/"))
+
+(use-package imenu
+  :straight nil
+  :after (:any markdown-mode yaml-mode prog-mode)
+  :custom
+  (imenu-auto-rescan t)
+  (imenu-max-items 1000)
+  ;; `t' will use a popup menu rather than a minibuffer prompt, `on-mouse' might be useful with
+  ;; mouse support enabled
+  (imenu-use-popup-menu nil)
+  ;; `nil' implies no sorting and will list by position in the buffer
+  (imenu-sort-function nil))
+
+(defvar tags-revert-without-query)
+
+(setq large-file-warning-threshold (* 500 1024 1024) ; MB
+      tags-add-tables nil
+      tags-case-fold-search nil ; t=case-insensitive, nil=case-sensitive
+      ;; Do not ask before rereading the `TAGS' files if they have changed
+      tags-revert-without-query t)
+
+(use-package xref
+:straight t
+  :commands xref-etags-mode
+  :bind
+  (("M-'"   . xref-find-definitions)
+   ("M-?"   . xref-find-references)
+   ("C-M-." . xref-find-apropos)
+   ("M-,"   . xref-pop-marker-stack)
+   :map xref--xref-buffer-mode-map
+   ("C-o"   . xref-show-location-at-point)
+   ("<tab>" . xref-quit-and-goto-xref)
+   ("r"     . xref-query-replace-in-results)))
+
+(use-package ivy-xref
+:straight t
+  :after (ivy xref)
+  :demand t
+  :custom
+  (xref-show-definitions-function #'ivy-xref-show-defs)
+  (xref-show-xrefs-function       #'ivy-xref-show-xrefs))
+
+(use-package counsel-etags
+:straight t
+  :defines (counsel-etags-ignore-directories counsel-etags-ignore-filenames)
+  :commands counsel-etags-virtual-update-tags
+  :if (and (symbol-value 'sb/IS-LINUX) (eq sb/minibuffer-completion 'ivy) (executable-find "ctags"))
+  :bind
+  (("M-]"     . counsel-etags-find-tag-at-point)
+   ("C-c g s" . counsel-etags-find-symbol-at-point)
+   ("C-c g f" . counsel-etags-find-tag)
+   ("C-c g l" . counsel-etags-list-tag)
+   ("C-c g c" . counsel-etags-scan-code))
+  :config
+  (defalias 'list-tags 'counsel-etags-list-tag-in-current-file)
+
+  (add-hook 'prog-mode-hook
+            (lambda ()
+              (add-hook 'after-save-hook #'counsel-etags-virtual-update-tags 'append 'local)))
+
+  (dolist (ignore-dirs '(".vscode" "build" ".metadata" ".recommenders" ".clangd" ".cache"))
+    (add-to-list 'counsel-etags-ignore-directories ignore-dirs))
+
+  (dolist (ignore-files '(".clang-format" ".clang-tidy" "*.json" "*.html" "*.xml"))
+    (add-to-list 'counsel-etags-ignore-filenames ignore-files)))
 
 (use-package dumb-jump
 :straight t
